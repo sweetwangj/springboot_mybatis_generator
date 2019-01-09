df612124617f1209a9ed4dd47f5fd292e4d94c6a
sweetwangj <37615979+sweetwangj@users.noreply.github.com>
2019-01-09 16:18:31 +0800

    springboot-mybatis+generator---demo-v0.1

---
+++ src/main/java/com/hans/boot_mybatis_generator_demo/BootMybatisGeneratorDemoApplication.java
@@ -0,0 +1,14 @@
+package com.hans.boot_mybatis_generator_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMybatisGeneratorDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootMybatisGeneratorDemoApplication.class, args);
+    }
+
+}
+
